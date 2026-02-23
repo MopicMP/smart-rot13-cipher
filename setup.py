f1162c60dd6a4e0f23f47ba8e1139d8516fc435a
RaccoNet <zavolokinyegor@gmail.com>
2026-02-23 18:02:37 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smart-rot13-cipher",
+    version="1.0.0",
+    description="Smart rot13 and caesar cipher with n shifts",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="raccoonette",
+    url="https://github.com/MopicMP/smart-rot13-cipher",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+    ],
+)
